9735989f758f68cca3dea95bd1dd61856f8a7350
Alex Hu <imalexhu@gmail.com>
2023-09-22 18:08:41 +0930

    Loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,24 @@ import { NewUserLandingPage } from "~/components/NewUserLandingPage"
 import { SearchPage } from "~/pages/search/index"
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { LoadingState } from "~/components/LoadingState";
 export default function Home() {
   const { isLoaded, isSignedIn, user } = useUser();
   const router = useRouter()
     
 
   useEffect(() => {
-    if (!user) {
+    if (user) {
       router.push("/search").catch((e)=>console.log(e))
     }
   }, [user]);
 
   // In case the user signs out while on the page.
-  // if (!isLoaded) {
-  //   return (
-  //     <Spinner/>
-  //   )
-  // }
+  if (!isLoaded) {
+    return (
+      <LoadingState/>
+    )
+  }
 
   if (!isSignedIn){
     return <NewUserLandingPage/>
