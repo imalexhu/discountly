4d1948aa8d6bcb4ff0b4643b73435f3a0cbdf1bb
Alex Hu <imalexhu@gmail.com>
2023-09-22 18:03:01 +0930

    File structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,22 @@
-import Head from "next/head";
-import Link from "next/link";
 import { SignInButton, UserButton, useUser} from "@clerk/nextjs";
-import { api } from "~/utils/api";
-
+import { NewUserLandingPage } from "~/components/NewUserLandingPage"
+import { SearchPage } from "~/pages/search/index"
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const { isLoaded, isSignedIn, user } = useUser();
 
+    
   // In case the user signs out while on the page.
-  if (!isLoaded || !isSignedIn) {
-    return (
-      <SignInButton/>
-    )
+  // if (!isLoaded) {
+  //   return (
+  //     <Spinner/>
+  //   )
+  // }
 
+  if (!isSignedIn){
+    return <NewUserLandingPage/>
   }
 
-  return (
-    <>
-    Hello, {user.firstName}
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
-      <p className="text-2xl text-black">
-        {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-      </p>
-    </>
-  );
+  return ( 
+    <SearchPage />
+  )
 }
